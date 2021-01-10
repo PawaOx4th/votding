fae368e143daddec2c793dc3097e3455477e0d01
pawaox4th <pawaox4th@gmail.com>
2021-01-10 15:27:12 +0700

    Create 404 Page Not Found

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,11 @@
   <div class="container">
     <div>
       <Logo />
-      {{ msg }}
+      <p>{{ msg }}</p>
+      <b-button>Button</b-button>
+      <b-button variant="danger">Button</b-button>
+      <b-button variant="success">Button</b-button>
+      <b-button variant="outline-primary">Button</b-button>
     </div>
   </div>
 </template>
@@ -31,35 +35,6 @@ export default Vue.extend({
 })
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style lang="scss" scoped>
+@import './pages/style.scss';
 </style>
